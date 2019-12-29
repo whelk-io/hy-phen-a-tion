34b17bf749449907a6e9e7281c18ca9d10da4fe4
Zack Teater <zteater@gmail.com>
2019-12-29 12:08:27 -0500

    initial class setup

---
+++ src/main/java/io/whelk/hy/phen/Hyphenator.java
@@ -0,0 +1,12 @@
+package io.whelk.hy.phen;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Hyphenator {
+  
+  public static void hyphen(String word) { 
+    // TODO
+  }
+
+}
