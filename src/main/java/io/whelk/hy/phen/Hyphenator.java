89cc52f194d769707c11a8187c4a8897c6561e56
Zack Teater <zteater@gmail.com>
2021-02-09 17:41:29 -0500

    update javadoc and add apache copyright, fixes #100, fixes #98

--- src/main/java/io/whelk/hy/phen/Hyphenator.java
+++ src/main/java/io/whelk/hy/phen/Hyphenator.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 Whelk Contributors (https://whelk.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.whelk.hy.phen;
 
 import static java.util.stream.Collectors.groupingBy;
@@ -13,48 +28,52 @@ import java.util.Map;
 import java.util.stream.IntStream;
 import lombok.SneakyThrows;
 
+/**
+ * @author Zack Teater
+ * @since 0.1.9
+ */
 public class Hyphenator {
-  
+
   private static Map<Integer, Map<String, List<Trie>>> tries;
   private static Map<String, Edge> edges;
-  
+
   private static int maxTrie;
   private static int minTrie;
 
-  static { 
+  static {
     loadHyphens();
     loadEdges();
   }
-  
+
   private static void loadHyphens() {
-      tries = Hyphenator
-                .readFromFile("/hyphen-en-us.txt")
-                .stream()
-                .sorted()
-                .distinct()
-                .map(Hyphenator::mapPattern)
-                .collect(groupingBy(t -> t.matcher().length(), groupingBy(t -> t.matcher())));
-      
-      var stats = tries.keySet().stream().mapToInt(i -> i).summaryStatistics();
-      maxTrie = stats.getMax();
-      minTrie = stats.getMin();
+    tries = Hyphenator //
+        .readFromFile("/hyphen-en-us.txt") //
+        .stream() //
+        .sorted() //
+        .distinct() //
+        .map(Hyphenator::mapPattern) //
+        .collect(groupingBy(t -> t.matcher().length(), groupingBy(t -> t.matcher())));
+
+    var stats = tries.keySet().stream().mapToInt(i -> i).summaryStatistics();
+    maxTrie = stats.getMax();
+    minTrie = stats.getMin();
   }
-  
+
   @SneakyThrows
   private static void loadEdges() {
-      edges = Hyphenator
-                .readFromFile("/edge-en-us.txt")
-                .stream()
-                .sorted()
-                .distinct()
-                .map(Hyphenator::mapEdge)
-                .collect(toMap(Edge::value, v -> v));
+    edges = Hyphenator //
+        .readFromFile("/edge-en-us.txt") //
+        .stream() //
+        .sorted() //
+        .distinct() //
+        .map(Hyphenator::mapEdge) //
+        .collect(toMap(Edge::value, v -> v));
   }
-  
+
   @SneakyThrows
-  private static List<String> readFromFile(String filename) { 
+  private static List<String> readFromFile(String filename) {
     var lines = new LinkedList<String>();
-    try(var inputStream = Hyphenator.class.getResourceAsStream(filename);
+    try (var inputStream = Hyphenator.class.getResourceAsStream(filename);
         BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
       String line;
       while ((line = br.readLine()) != null) {
@@ -63,7 +82,7 @@ public class Hyphenator {
     }
     return lines;
   }
-  
+
   private static Trie mapPattern(String value) {
     var plainText = value.replaceAll("[\\.0-9]", "");
     var isLeadingMatch = value.startsWith(".");
@@ -72,30 +91,30 @@ public class Hyphenator {
     var trieArr = toTrieArray(value, plainText, isTrailingWildcard);
     return new Trie(value, plainText, trieArr, isLeadingMatch, isTrailingMatch);
   }
-  
-  private static Edge mapEdge(String value) { 
+
+  private static Edge mapEdge(String value) {
     return new Edge(value.replaceAll("-", ""), value, Arrays.asList(value.split("-")));
   }
-  
-  private static char[] toTrieArray(String value, String plainText, boolean isTrailingWildcard) { 
+
+  private static char[] toTrieArray(String value, String plainText, boolean isTrailingWildcard) {
     var trimmed = value.replaceAll("\\.", "");
     var len = plainText.length();
     if (isTrailingWildcard)
       len++;
     var arr = new char[len * 2];
     Arrays.fill(arr, '0');
-    
+
     int i = 0;
-    for (char c : trimmed.toCharArray()) { 
+    for (char c : trimmed.toCharArray()) {
       if (c >= '0' && c <= '9') {
         if (i % 2 == 0)
           arr[i] = c;
         else
           arr[++i] = c;
       } else {
-        if (i %2 == 0)
+        if (i % 2 == 0)
           arr[++i] = c;
-        else 
+        else
           arr[i] = c;
       }
       i++;
@@ -104,20 +123,20 @@ public class Hyphenator {
   }
 
   /**
-   * Try to calculate where hyphens should be placed (which in turn is in between syllables) using
-   * TEX82 hyphenation patterns. This is largely based on Frank Liang's dictoral thesis from 1983
-   * titled 'Word Hy-phen-a-tion by Com-put-er'.
+   * Try to calculate where hyphens should be placed (which in turn is in between
+   * syllables) using TEX82 hyphenation patterns. This is largely based on Frank
+   * Liang's dictoral thesis from 1983 titled 'Word Hy-phen-a-tion by Com-put-er'.
    * 
-   * @param word
+   * @param word to split into hyphens using syllables
    * @return HyphenResult
    */
   public static HyphenResult hyphen(String word) {
     validate(word);
-    
+
     // convert word
     word = word.toLowerCase();
     var result = new HyphenResult().originalWord(word);
-    
+
     // check if edge case
     if (edges.containsKey(word)) {
       var edge = edges.get(word);
@@ -146,7 +165,7 @@ public class Hyphenator {
         var sub = word.substring(j, j + i);
         if (trieMap.containsKey(sub)) {
           var tries = trieMap.get(sub);
-          for (Trie trie : tries) { 
+          for (Trie trie : tries) {
             if (trie.isLeadingMatch() && !isLeadingMatchOnly)
               continue;
             if (trie.isTrailingMatch() && !isTrailingMatchOnly)
@@ -158,21 +177,21 @@ public class Hyphenator {
         }
       }
     }
-    
+
     // update result
     result.tries(matchedTrie);
     result.trieWord(groupByToken(wordTrieArr));
     var syllables = groupBySyllable(wordTrieArr);
     result.syllables(syllables);
     result.hyphenWord(hyphenWord(syllables));
-    
+
     return result;
   }
 
   private static void validate(String word) {
     if (word == null || word.trim().isEmpty())
       throw new IllegalArgumentException("word cannot be null or blank");
-    
+
     if (!word.matches("^[a-zA-Z]+$"))
       throw new IllegalArgumentException("word must contain alpha characters only");
   }
@@ -184,26 +203,24 @@ public class Hyphenator {
    * @return
    */
   private static String hyphenWord(List<String> syllables) {
-    return syllables
-            .stream()
-            .collect(joining("-"));
+    return syllables.stream().collect(joining("-"));
   }
-  
+
   /**
    * Join wordTrieArr into final trie model with scores.
    * 
    * @param wordTrieArr
    * @return
    */
-  private static String groupByToken(char[] wordTrieArr) { 
-    return IntStream
-            .range(1, wordTrieArr.length)
-            .mapToObj(i -> wordTrieArr[i])
-            .map(c -> c.toString())
-            .filter(c -> !"0".equals(c))
-            .collect(joining());
+  private static String groupByToken(char[] wordTrieArr) {
+    return IntStream //
+        .range(1, wordTrieArr.length) //
+        .mapToObj(i -> wordTrieArr[i]) //
+        .map(c -> c.toString()) //
+        .filter(c -> !"0".equals(c)) //
+        .collect(joining());
   }
-  
+
   /**
    * Break wordTrie by odd-value scores
    * 
@@ -213,7 +230,7 @@ public class Hyphenator {
   private static List<String> groupBySyllable(char[] wordTrieArr) {
     List<String> syllables = new LinkedList<>();
     int start = 1;
-    for (int i = 2; i < wordTrieArr.length; i += 2) { 
+    for (int i = 2; i < wordTrieArr.length; i += 2) {
       if (wordTrieArr[i] % 2 != 0) {
         syllables.add(joinWord(wordTrieArr, start, i));
         start = i;
@@ -222,7 +239,7 @@ public class Hyphenator {
     syllables.add(joinWord(wordTrieArr, start, wordTrieArr.length));
     return syllables;
   }
-  
+
   /**
    * Join char arr into String.
    * 
@@ -231,13 +248,13 @@ public class Hyphenator {
    * @param end
    * @return
    */
-  private static String joinWord(char[] wordTrieArr, int start, int end) { 
-    return IntStream
-            .range(start, end)
-            .filter(j -> j % 2 != 0)
-            .mapToObj(j -> wordTrieArr[j])
-            .map(c -> c.toString())
-            .collect(joining());
+  private static String joinWord(char[] wordTrieArr, int start, int end) {
+    return IntStream //
+        .range(start, end) //
+        .filter(j -> j % 2 != 0) //
+        .mapToObj(j -> wordTrieArr[j]) //
+        .map(c -> c.toString()) //
+        .collect(joining());
   }
 
   /**
--- src/main/java/io/whelk/hy/phen/Trie.java
+++ src/main/java/io/whelk/hy/phen/Trie.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 Whelk Contributors (https://whelk.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.whelk.hy.phen;
 
 import lombok.Data;
@@ -5,22 +20,26 @@ import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.Accessors;
 
+/**
+ * @author Zack Teater
+ * @since 0.1.9
+ */
 @Data
 @RequiredArgsConstructor
 @Accessors(fluent = true)
 public class Trie {
 
   private final String value;
-  
+
   @ToString.Exclude
   private final String matcher;
-  
+
   @ToString.Exclude
   private final char[] arr;
-  
+
   @ToString.Exclude
   private final boolean isLeadingMatch;
-  
+
   @ToString.Exclude
   private final boolean isTrailingMatch;
 
